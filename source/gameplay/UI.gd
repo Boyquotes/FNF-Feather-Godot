cca70b83bce0ae07474992b2a1d885500887298e
Gabriela <93128334+BeastlyGabi@users.noreply.github.com>
2023-04-14 22:11:12 -0400

    oops

--- source/gameplay/UI.gd
+++ source/gameplay/UI.gd
@@ -2,5 +2,5 @@ extends Control
 
 @onready var scoreText : Label = $"Score Text"
 @onready var cpuText : Label = $"CPU Text"
-@onready var iconPL : Sprite2D = $iconPL
-@onready var iconOPP : Sprite2D = $iconOPP
+@onready var iconPL : Sprite2D = $"Health Bar/iconPL"
+@onready var iconOPP : Sprite2D = $"Health Bar/iconOPP"
