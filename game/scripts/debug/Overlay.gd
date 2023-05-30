1774460a61ddecc802062678dbb962a095c9247d
Gabriela <93128334+BeastlyGabi@users.noreply.github.com>
2023-05-29 22:46:42 -0400

    Update Overlay.gd

--- game/scripts/debug/Overlay.gd
+++ game/scripts/debug/Overlay.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 func _process(_delta:float):
-	$FPS.text = "FPS: ["+str(Engine.get_frames_per_second()) + "]"
+	$FPS.text = "FPS: "+str(Engine.get_frames_per_second())
 	$FPS.text += "\nRAM USAGE: "+String.humanize_size(OS.get_static_memory_usage()).to_lower()
 	$FPS.text += "\nRAM PEAK: "+String.humanize_size(OS.get_static_memory_peak_usage()).to_lower()
 
