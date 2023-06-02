8ab99f0cd9d749375f4016a08f34306b81aea015
Gabriela <93128334+BeastlyGabi@users.noreply.github.com>
2023-06-01 21:07:57 -0400

    descriptions I forgor

--- game/scripts/menus/data/GameOption.gd
+++ game/scripts/menus/data/GameOption.gd
@@ -2,6 +2,7 @@ class_name GameOption extends Resource
 
 @export var option:String
 @export var reference:String
+@export var description:String
 
 var value:Variant:
 	get: return Settings.get_setting(reference)
