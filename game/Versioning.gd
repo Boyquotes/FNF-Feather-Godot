c6707ccb1558bb16f51731bc5ca8787d3a494403
Gabriela Bestial <93128334+BeastlyGabi@users.noreply.github.com>
2023-06-10 00:51:49 -0400

    Update Versioning.gd

--- game/Versioning.gd
+++ game/Versioning.gd
@@ -1,14 +1,15 @@
 class_name Versioning
 
-enum VersionType {STABLE, NIGHTLY, BLEEDING}
+# Stable, Beta, Deg
+enum VersionType {IRIS, LUNA, LILYPAD}
 
 const GAME_VERSION:String = "0.01"
 const FNF_VERSION:String = "0.2.8"
-const VER_TYPE:VersionType = VersionType.BLEEDING
+const VER_TYPE:VersionType = VersionType.LILYPAD
 
 static func grab_schema_name() -> String:
 	match VER_TYPE:
-		# VersionType.STABLE: return "[Stable]"
-		VersionType.NIGHTLY: return "[Nightly]"
-		VersionType.BLEEDING: return "[Bleeding]"
+		# VersionType.IRIS: return "[Iris]"
+		VersionType.LUNA: return "[Luna]"
+		VersionType.LILYPAD: return "[Lilypad]"
 		_: return ""
